0e013ba6688695276645eeb36960ea7d6db0d03b
Kirill <git@good.cpp>
2019-02-06 22:12:31 -0500

    Added functional drop down menu for navbar for mobile. Increased fontsize.

--- static/main.js
+++ static/main.js
@@ -9,20 +9,34 @@ function optimizeForMobile(){
     let navbar_buttons = document.getElementById('navbar-buttons');
     let navbar = document.getElementById('navbar');
     let optimized = document.getElementById('dropnavbar-content')?true:false;
-    let dropDownTagStart = "<button class=\"dropnavbar__button navbar__button navbar__text\"> <i class=\"material-icons\"> dehaze </i> "+
+    let dropDownTagStart = "<button onclick=\"dropNavBarClick()\" "+
+                            "class=\"dropnavbar__button navbar__text\" id=\"dropnavbar__button\">"+
+                            "<i class=\"material-icons\"> dehaze </i> "+
                             window.location.pathname.replace(/\//g,'') + 
                             "</button><div class=\"dropnavbar__content\" id=\"dropnavbar-content\">";
     let dropDownTagEnd = "</div>";
     if (optimizeAtWidth > window.innerWidth ) {
         if (!optimized) {
-            navbar_buttons.innerHTML = dropDownTagStart + navbar_buttons.innerHTML + dropDownTagEnd
-            navbar.style.justifyContent ="flex-start"
+            navbar_buttons.innerHTML = dropDownTagStart + navbar_buttons.innerHTML + dropDownTagEnd;
+            navbar.style.justifyContent ="space-around";
         }
     }
     else {
         navbar_buttons.innerHTML = navbar_buttons.innerHTML.replace(dropDownTagStart,'');
-        navbar_buttons.innerHTML = navbar.innerHTML.replace(dropDownTagEnd,'');
+        navbar_buttons.innerHTML = navbar_buttons.innerHTML.replace(dropDownTagEnd,'');
 
     }
+}
+
+function dropNavBarClick(){
+    document.getElementById('dropnavbar-content').classList.toggle("show"); 
+    for (let el of document.getElementsByClassName('navbar__button')) {
+        el.classList.toggle("dropnavbar__button--borders")
+    }
+    
+}
+
+function dropDownOnClick() {
+    console.log("Test")
 }
 
